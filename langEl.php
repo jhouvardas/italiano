f06d8a47a0e4edfc2d4be494760a8677086cf11f
jhouvardas <jhouvardas@gmail.com>
2022-02-23 08:00:39 +0200

    starting multilingual

---
+++ langEl.php
@@ -0,0 +1,16 @@
+<?php
+
+define("_SEARCHWORD", "Αναζήτηση λέξης");
+define("_TEST", "Τεστ");
+define("_YOURLANGUAGEITALIAN", "Ελληνικά Ιταλικά");
+define("_ITALIANYOURLANGUAGE", "Ιταλικά Ελληνικά");
+define("_VERBCONJUGATIONS", "Κλίσεις ρημάτων");
+define("_NOTES", "Σημειώσεις");
+define("_ADDWORD", "Εισαγωγή λέξης");
+define("_EDITWORD", "Διόρθωση λέξης");
+define("_ADDVERB", "Εισαγωγή ρήματος");
+define("_ADDVERBCONJUGATION", "Εισαγωγή κλίσης ρήματος");
+define("_ADDSENTENCE", "Εισαγωγή πρότασης");
+define("_EDITSENTENCE", "Διόρθωση πρότασης");
+
+
